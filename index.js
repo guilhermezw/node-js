ea53b40ff3cce6e306ea6c5b016982789af50a3d
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-03-16 20:48:53 -0300

    Criando modelo de usuário

--- index.js
+++ index.js
@@ -7,3 +7,25 @@ const sequelize = new Sequelize('db_projeto' , 'root' , '' ,{
     host: 'localhost',
     dialect: 'mysql'
 })
+
+const Usuario = sequelize.define('Usuario' , {
+    id:{
+        type: DataTypes.UUID,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    nome: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    telefone: {
+        type: DataTypes.STRING,
+        unique: true
+    }
+        
+})
