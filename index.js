c9aa49a45d079fb888584e84f3d10da149d2bcfc
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-03-16 21:28:21 -0300

    Endpoint para criação de Usuários

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ const sequelize = new Sequelize('db_projeto' , 'root' , '' ,{
     dialect: 'mysql'
 })
 
+//Criação do modelo do Usuário
 const Usuario = sequelize.define('Usuario' , {
     id:{
-        type: DataTypes.UUID,
+        type: DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true
     },
@@ -28,4 +29,45 @@ const Usuario = sequelize.define('Usuario' , {
         unique: true
     }
         
+})
+
+// Configuração do servidor express.
+const app = express()
+app.use(cors()) // Permite o front-end acessar a API.
+app.use(express.json()) //Permite o servidor entender JSON.
+
+const port = 3001
+
+// Definição de rotas (endpoints).
+// req: request
+// res: response
+app.get('/usuarios' , async (request , response) =>{
+    const listarUsuarios = await Usuario.findAll()
+    response.json(listarUsuarios)
+})
+
+app.post('/usuarios' , async(request , response) => {
+    try{
+        const {nome , email  , telefone} = request.body
+        const salvarUsuario = await Usuario.create({nome , email , telefone})
+        response.status(201).json({
+            message: "Usuário criado.",
+            usuario: salvarUsuario
+        })
+    } catch (error){
+        response.status(400).json({
+            erro: 'Erro ao cadastrar Usuário.'
+        })
+    }
+})
+
+// Iniciando o servidor.
+sequelize.sync().then(() =>{
+    app.listen(port , () =>{
+        console.log(`Servidor rodando na porta: ${port}`)
+        console.log(`Banco de dados sincronizado`)
+    })
+}).catch((error) => {
+    console.log('Erro ao conectar ou sincronizar com bancos de dados.')
+    console.error(error)
 })
