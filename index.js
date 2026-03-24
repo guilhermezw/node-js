ea5d386e186c51a317ebccb7424de0887971bcf2
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-03-23 21:07:23 -0300

    .

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const sequelize = new Sequelize('db_projeto' , 'root' , '' ,{
     dialect: 'mysql'
 })
 
-// Definição de tabelas (modelo) do Usuário
-const Usuario = sequelize.define('Usuario' , {
+// Definição de tabelas (modelo) do Funcionario
+const Funcionario = sequelize.define('Funcionario' , {
     id:{
         type: DataTypes.INTEGER,
         primaryKey: true,
@@ -19,16 +19,46 @@ const Usuario = sequelize.define('Usuario' , {
         type: DataTypes.STRING,
         allowNull: false
     },
-    email: {
+    cpf: {
         type: DataTypes.STRING,
         allowNull: false,
         unique: true
     },
-    telefone: {
+    dataNascimento: {
+        type: DataTypes.DATEONLY,
+        allowNull: false
+    },
+    email:{
         type: DataTypes.STRING,
+        allowNull: false,
         unique: true
     }
         
+})
+
+// Definição de tabelas (modelo) do Livro
+const Livro = sequelize.define('Livro' , {
+    id:{
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    titulo:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    autor:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    numeroPaginas:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    preco:{
+        type: DataTypes.DOUBLE,
+        allowNull: false
+    }
 })
 
 // Configuração do servidor express.
@@ -41,65 +71,129 @@ const port = 3001
 // Definição de rotas (endpoints).
 // req: request
 // res: response
-app.get('/usuarios' , async (request , response) =>{
-    const listarUsuarios = await Usuario.findAll()
-    response.json(listarUsuarios)
+app.get('/funcionarios' , async (request , response) =>{
+    const listarFuncionarios = await Funcionario.findAll()
+    response.json(listarFuncionarios)
 })
 
-app.post('/usuarios' , async(request , response) => {
+app.post('/funcionarios' , async(request , response) => {
     try{
-        const {nome , email  , telefone} = request.body
-        const salvarUsuario = await Usuario.create({nome , email , telefone})
+        const {nome , cpf  , dataNascimento , email} = request.body
+        const salvarFuncionario = await Funcionario.create({nome , cpf , dataNascimento , email})
         response.status(201).json({
-            message: "Usuário criado.",
-            usuario: salvarUsuario
+            message: "Funcionário criado.",
+            funcionario: salvarFuncionario
         })
     } catch (error){
         response.status(400).json({
-            erro: 'Erro ao cadastrar Usuário.'
+            erro: 'Erro ao criar Funcionário.'
         })
     }
 })
 
-app.put("/usuarios/:id" , async (request , response) => {
+app.put("/funcionarios/:id" , async (request , response) => {
     try{
         const {id} = request.params
-        const {nome , email , telefone} = request.body
+        const {nome , cpf , dataNascimento , email} = request.body
 
-        const[updated] = await Usuario.update(
-            {nome , email , telefone},
+        const[updated] = await Funcionario.update(
+            {nome , cpf , dataNascimento , email},
             {where: {id: id}}
         )
 
         if(updated){
-            const usuarioAtualizado = await Usuario.findByPk(id)
+            const funcionarioAtualizado = await Funcionario.findByPk(id)
             return response.status(200).json({
-                mensage: "Usuário atualizado com sucesso.",
-                usuario: usuarioAtualizado
+                message: "Funcionário atualizado com sucesso.",
+                funcionario: funcionarioAtualizado
             })
         }
 
-        return response.status(404).json({erro: "Usuário não encontrado."})
+        return response.status(404).json({erro: "Funcionário não encontrado."})
 
     } catch (error){
-        response.status(500).json({erro: "Erro ao atualizar usuário."})
+        response.status(500).json({erro: "Erro ao atualizar funcionário."})
     }
 })
 
-app.delete("/usuarios/:id" , async (request , response) => {
+app.delete("/funcionarios/:id" , async (request , response) => {
     try{
         const {id} = request.params
-        const deleted = await Usuario.destroy( {where: {id: id}})
+        const deleted = await Funcionario.destroy( {where: {id: id}})
 
         if(deleted){
-            return response.status(200).json({message: "Usuário deletado"})
+            return response.status(200).json({message: "Funcionário deletado."})
         }
 
-        return response.status(404).json({message: "Usuário não encontrado"})
+        return response.status(404).json({message: "Funcionário não encontrado."})
     } catch(error){
-        response.status(500).json({erro: "Erro ao deletar usuário."})
+        response.status(500).json({erro: "Erro ao deletar funcionário."})
     }
 })
+
+
+// Endpoint - Livro
+
+app.get('/livros' , async (request , response) =>{
+    const listarLivros = await Livro.findAll()
+    response.json(listarLivros)
+})
+
+app.post('/livros' , async(request , response) => {
+    try{
+        const {titulo , autor  , numeroPaginas , preco} = request.body
+        const salvarLivro = await Livro.create({titulo , autor , numeroPaginas , preco})
+        response.status(201).json({
+            message: "Livro criado.",
+            livro: salvarLivro
+        })
+    } catch (error){
+        response.status(400).json({
+            erro: 'Erro ao criar livro.'
+        })
+    }
+})
+
+app.put("/livros/:id" , async (request , response) => {
+    try{
+        const {id} = request.params
+        const {titulo , autor , numeroPaginas , preco} = request.body
+
+        const[updated] = await Livro.update(
+            {titulo , autor , numeroPaginas , preco},
+            {where: {id: id}}
+        )
+
+        if(updated){
+            const livroAtualizado = await Livro.findByPk(id)
+            return response.status(200).json({
+                mensage: "Livro atualizado com sucesso.",
+                livro: livroAtualizado
+            })
+        }
+
+        return response.status(404).json({erro: "Livro não encontrado."})
+
+    } catch (error){
+        response.status(500).json({erro: "Erro ao atualizar livro."})
+    }
+})
+
+app.delete("/livros/:id" , async (request , response) => {
+    try{
+        const {id} = request.params
+        const deleted = await Livro.destroy( {where: {id: id}})
+
+        if(deleted){
+            return response.status(200).json({message: "Livro deletado"})
+        }
+
+        return response.status(404).json({message: "Livro não encontrado"})
+    } catch(error){
+        response.status(500).json({erro: "Erro ao deletar livro."})
+    }
+})
+
 
 // Iniciando o servidor.
 sequelize.sync().then(() =>{
